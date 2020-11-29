c04bf0b425178cfcc1579c3e00b8bcba5485bace
eric-merz <69837919+eric-merz@users.noreply.github.com>
2020-11-29 15:05:26 +1100

    fixed link

--- templates/simple-header-layout.html.php
+++ templates/simple-header-layout.html.php
@@ -98,7 +98,7 @@
                 $categoryId = $row["categoryId"];
                 ?>
                 <li class="footer-category">
-                  <a href="browse-category.php?=<?= $categoryId ?>"><?= $categoryName ?></a>
+                  <a href="browse-category.php?categoryId=<?= $categoryId ?>"><?= $categoryName ?></a>
                 </li>
                 <?php endforeach; ?>
               </ul>
